88a95d00e4ebf9eb2720dd6ba83fb6fd101200b4
Alex Klick <aklick12@gmail.com>
2021-09-24 14:38:26 -0600

    forecast services setup, geo_loc refactored

--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'WeatherService' do
+RSpec.describe 'ForcastService' do
   describe 'get_weather' do
     it 'take an array of [lat, lng] and returns the parsed weather data' do
-      weather_data = WeatherService.get_forecast([39.738453, -104.984853])
+      forecast_data = ForecastService.get_forecast([39.738453, -104.984853])
       expect().to eq(content)
     end
   end

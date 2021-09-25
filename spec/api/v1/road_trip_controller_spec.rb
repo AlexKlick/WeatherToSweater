1d4b6fb39b47c77cd7395c44aeb3930404be9ab1
Alex Klick <aklick12@gmail.com>
2021-09-25 17:17:10 -0600

    initial spec

---
+++ spec/api/v1/road_trip_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::RoadTripController do
+  describe 'Create' do
+    it 'takes in a Road Trip data object and creates a road trip' do
+    
+    end
+  end
+end
---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast Facade' do
+  describe '' do
+
+  end
+end

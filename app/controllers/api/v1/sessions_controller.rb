a2bc69656775afb5892cb595cf448c5a6b7b1c82
Alex Klick <aklick12@gmail.com>
2021-09-25 16:20:27 -0600

    creates session and returns data, tests for correct and sad paths

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,22 @@
-class Api::V1::SessionsController
+class Api::V1::SessionsController < ApplicationController
   def create
-    
+    user = User.find_by(email: params[:email])
+    if user == nil
+      render json: "email and passwords don't match".to_json, status: 401
+    else
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        serial_hash = {}
+        serial_hash[:data] = {}
+        serial_hash[:data][:type] = "users"
+        serial_hash[:data][:id] = user.id
+        serial_hash[:data][:attributes] = {}
+        serial_hash[:data][:attributes][:email] = user.email
+        serial_hash[:data][:attributes][:api_key] = user.api_key
+        render json: serial_hash, status: 201
+      else
+        render json: "email and passwords don't match".to_json, status: 401
+      end
+    end
   end
 end

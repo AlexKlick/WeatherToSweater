6bc2b47aabafbb37f54bf346e3d4ff83348c96b5
Alex Klick <aklick12@gmail.com>
2021-09-27 11:22:29 -0600

    controller test passes. all tests within final_tests folder. run bundle exec rspec spec/api/v1/final_tests to see all test for booksearch, open coverage/index.html shows all book_search files are tested and passing

--- spec/api/v1/final_tests/book-search_controller_spec.rb
+++ spec/api/v1/final_tests/book-search_controller_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'Book-search Controller' do
-  describe 'feature_location' do
-
+RSpec.describe 'BookSearch Controller' do
+  describe 'index' do
+    it 'renders the serialized book_search data to the client in json' do
+      get '/api/v1/book-search?location=denver,co&quantity=5'
+      res = JSON.parse(response.body,symbolize_names: true) #implicitly tests if JSON because if not, then it will error out
+      expect(res).to be_a(Hash) # already tested twice that the hash being sent to controller is in the correct format with all data required present
+    end
   end
 end

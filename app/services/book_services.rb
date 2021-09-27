7c99d9a9ce03735769081120b6904c32f740f880
Alex Klick <aklick12@gmail.com>
2021-09-27 09:24:10 -0600

    bookservices returns books with the city in the title, passes test

--- app/services/book_services.rb
+++ app/services/book_services.rb
@@ -1,4 +1,7 @@
 module BookServices
   #https://openlibrary.org/search.json?title=
-  
+  def self.get_books_for_location(location)
+    response = Faraday.get("https://openlibrary.org/search.json?title=#{location}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end

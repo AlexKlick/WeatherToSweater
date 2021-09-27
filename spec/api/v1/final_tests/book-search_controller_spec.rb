b316f6e87637a02c576f42f26673a1eac1c1ea3c
Alex Klick <aklick12@gmail.com>
2021-09-27 11:35:31 -0600

    updated serializer to return empty array if books isbn is not present in book_service response

--- spec/api/v1/final_tests/book-search_controller_spec.rb
+++ spec/api/v1/final_tests/book-search_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'BookSearch Controller' do
     it 'renders the serialized book_search data to the client in json' do
       get '/api/v1/book-search?location=denver,co&quantity=5'
       res = JSON.parse(response.body,symbolize_names: true) #implicitly tests if JSON because if not, then it will error out
-      expect(res).to be_a(Hash) # already tested twice that the hash being sent to controller is in the correct format with all data required present
+      expect(res).to be_a(Hash) # already tested twice that the hash being sent to controller is in the correct format with all data required present, with the correct type
     end
   end
 end

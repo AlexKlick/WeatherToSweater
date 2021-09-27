2a38557c7b62f80060da686da83aab06e66a321d
Alex Klick <aklick12@gmail.com>
2021-09-27 09:17:34 -0600

    books services test setup

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Book Services' do
-  describe 'feature_location' do
-
+  describe 'get_books_for_location(location)' do
+    it 'returns books that reference the location' do
+      location = "minneapolis,Mn"
+      books = BookServices.get_books_for_location(location)
+      books = JSON.parse(books.body, symbolize_names: true)
+      binding.pry
+      expect(books[0]).to eq(content)
+    end
   end
 end

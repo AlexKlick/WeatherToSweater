187a03da004753170ecf03c590aba22cf56eb043
Alex Klick <aklick12@gmail.com>
2021-09-27 10:24:44 -0600

    added limit to service test

--- app/services/book_services.rb
+++ app/services/book_services.rb
@@ -1,7 +1,7 @@
 module BookServices
   #https://openlibrary.org/search.json?title=
-  def self.get_books_for_location(location)
-    response = Faraday.get("https://openlibrary.org/search.json?title=#{location}")
+  def self.get_books_for_location(location, limit)
+    response = Faraday.get("https://openlibrary.org/search.json?title=#{location}&limit=#{limit}")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Book Services' do
-  describe 'get_books_for_location(location)' do
+  before(:each) do    
+    @location = "minneapolis,Mn"
+    @limit = 5
+    @books = BookServices.get_books_for_location(@location, @limit)
+  end
+  describe 'get_books_for_location(location, limit)' do
     it 'returns books that reference the location' do
-      location = "minneapolis,Mn"
-      books = BookServices.get_books_for_location(location)
-      books[:docs][0][:title].include?("Minneapolis")
-      expect(books[:docs][0][:title]).to include("Minneapolis")
-      expect(books[:docs][-1][:title]).to include("Minneapolis")
+      expect(@books[:docs][0][:title]).to include("Minneapolis")
+      expect(@books[:docs][-1][:title]).to include("Minneapolis")
+    end
+    
+    it 'returns the number of books equal to or lower than (if total books is less than limit) limit' do
+      expect(@books[:docs].length).to eq(@limit)
     end
   end
 end
